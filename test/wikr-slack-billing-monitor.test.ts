9a8dda20a89b67b9d72239e88447e0a14d09fb13
Kevin Rich <whoiskevinrich@gmail.com>
2024-01-26 22:18:08 -0800

    init cdk

---
+++ test/wikr-slack-billing-monitor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WikrSlackBillingMonitor from '../lib/wikr-slack-billing-monitor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wikr-slack-billing-monitor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WikrSlackBillingMonitor.WikrSlackBillingMonitorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
